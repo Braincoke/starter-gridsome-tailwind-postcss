b9af8499c5e310db5bdf73516010c852ad55ff50
Braincoke <braincoke@protonmail.com>
2020-03-04 22:23:41 +0100

    Add and configure PurgeCSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,13 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const tailwind = require("tailwindcss");
 
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 const postcssPlugins = [tailwind()];
 
+if (process.env.NODE_ENV === "production")
+  postcssPlugins.push(purgecss(require("./purgecss.config.js")));
+  
 module.exports = {
   siteName: "Gridsome",
   plugins: [],
