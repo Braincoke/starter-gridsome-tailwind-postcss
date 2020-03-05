ec8b74bd34973cd3ee35d4bec18545a8794455a7
Braincoke <braincoke@protonmail.com>
2020-03-05 09:31:59 +0100

    Replace postcss-cssnext by postcss-preset-env

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,14 +4,23 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const tailwind = require("tailwindcss");
-const cssnext = require("postcss-cssnext")
 const purgecss = require("@fullhuman/postcss-purgecss");
+const presetEnv = require("postcss-preset-env");
 
-const postcssPlugins = [ cssnext(), tailwind()];
+const postcssPlugins = [
+  presetEnv({
+    /* use stage 3 features + css nesting rules */
+    stage: 3,
+    features: {
+      "nesting-rules": true
+    }
+  }),
+  tailwind()
+];
 
 if (process.env.NODE_ENV === "production")
   postcssPlugins.push(purgecss(require("./purgecss.config.js")));
-  
+
 module.exports = {
   siteName: "Gridsome",
   plugins: [],
